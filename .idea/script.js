bee32b87012deeba36518c9de626ae05a0dd80e0
mari2791 <marijalaghonia@gmail.com>
2025-05-28 13:52:11 +0400

    js file edited

---
+++ .idea/script.js
@@ -0,0 +1,11 @@
+function checkIfEligible(age) {
+    if (typeof age !== 'number' || isNaN(age)) {
+        console.log("მითითებული ასაკი არასწორია!");
+    } else if (age < 18) {
+        console.log("თქვენ არ შეგიძლიათ მართვის მოწმობის აღება");
+    } else {
+        console.log("თქვენ შეგიძლიათ მართვის მოწმობის აღება");
+    }
+}
+
+
