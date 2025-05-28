3a9dd2af8658eb61f9f340ee9966eab5d3d4d3d7
mari2791 <marijalaghonia@gmail.com>
2025-05-28 14:30:38 +0400

    errors corrected

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-function checkIfEligible(age) {
-    if (typeof age !== 'number') {
-        console.log("მითითებული ასაკი არასწორია!");
-    } else if (isNaN(age)) {
-        console.log("მითითებული ასაკი არასწორია!");
-    } else if (age < 18) {
-        console.log("თქვენ არ შეგიძლიათ მართვის მოწმობის აღება");
-    } else if (age >= 18) {
-        console.log("თქვენ შეგიძლიათ მართვის მოწმობის აღება");
-    }
-}
+let age = 15; // შეგიძლიათ შეცვალოთ ეს მნიშვნელობა
 
-checkIfEligible("17"); // "თქვენ არ შეგიძლიათ მართვის მოწმობის აღება"
-checkIfEligible("abc"); // "მითითებული ასაკი არასწორია!"
-checkIfEligible("25"); // "თქვენ შეგიძლიათ მართვის მოწმობის აღება"
-checkIfEligible(25); // "თქვენ შეგიძლიათ მართვის მოწმობის აღება"
+if (typeof age !== "number" || isNaN(age)) {
+    console.log("მითითებული ასაკი არასწორია!");
+} else if (age < 18) {
+    console.log("თქვენ არ შეგიძლიათ მართვის მოწმობის აღება");
+} else {
+    console.log("თქვენ შეგიძლიათ მართვის მოწმობის აღება");
+}
